21eec299c326b8fc40d977dfb6c43c760de2cc55
spoorjith1 <spoorjith1@gmail.com>
2026-06-21 20:29:13 +0530

    delete page

--- frontend-react/src/pages/AccountDeletion.jsx
+++ frontend-react/src/pages/AccountDeletion.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function AccountDeletion() {
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <h3>Delete Account</h3>
+    <div className='page-container page'>
+      <div className="container">
+        <h3 className='delete-title'>Delete your Account</h3>
+        <div className='delete-box'>
+          <p><b>NOTE :</b> by deleting your account, your whole data from this application will permanently be deleted. and you cannot retrieve the data in the future. if you think you did a mistake you will have next <b>30 Days</b> to login back to your account and "Cancel the Account Deletion". after 30 Days the account will permanently removed from our database.</p>
+          <div className='delete-btns-box'>
+            <button className='del-cancel-btn' onClick={()=> navigate('/')}>Cancel</button>
+            <button className='del-confirm-btn'>Yes, Delete the Account</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
